2eb8eaf3cffbc10502a7e6e08bd4fac7556fcacd
Mushinako <ridoedee@gmail.com>
2019-04-28 13:25:46 -0700

    1B-1 improvement
    Still no large set :worried:

--- 03-1B/01-Manhattan_Crepe_Cart/solution-PF.py
+++ 03-1B/01-Manhattan_Crepe_Cart/solution-PF.py
@@ -1,44 +1,28 @@
 #!/usr/bin/env python3
-
-
-# Get the smallest position of maximum number of a 2d-array
-def getmaxcoord(man, q):
-    most = max(map(max, man))
-    for j in range(q):
-        for i in range(q):
-            if man[i][j] == most:
-                return (j, i)
-
-
 def main():
     t = int(input())
     for k in range(1, t+1):
         p, q = [int(x) for x in input().split()]
         q += 1
-        # Set up a 2d-array of intersections, and count the number of people
-        #   who potentially goes to that section
-        man = [[0] * q for _ in range(q)]
+        # Consider x- and y- directions independently
+        xman = [0] * q
+        yman = [0] * q
         for _ in range(p):
-            xcoord, ycoord, dire = input().split()
-            x = int(xcoord)
-            y = int(ycoord)
+            x, y, dire = input().split()
             if dire == 'N':
-                for i in range(y+1, q):
-                    for j in range(q):
-                        man[i][j] += 1
+                for i in range(int(y)+1, q):
+                    yman[i] += 1
             elif dire == 'S':
-                for i in range(y):
-                    for j in range(q):
-                        man[i][j] += 1
+                for i in range(int(y)):
+                    yman[i] += 1
             elif dire == 'E':
-                for i in range(q):
-                    for j in range(x+1, q):
-                        man[i][j] += 1
+                for i in range(int(x)+1, q):
+                    xman[i] += 1
             else:
-                for i in range(q):
-                    for j in range(x):
-                        man[i][j] += 1
-        x, y = getmaxcoord(man, q)
+                for i in range(int(x)):
+                    xman[i] += 1
+        x = xman.index(max(xman))
+        y = yman.index(max(yman))
         print('Case #{0}: {1} {2}'.format(k, x, y))
 
 
